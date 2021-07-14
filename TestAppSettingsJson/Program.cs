ee2d0651b77bd7dc2fa701a9a13a791671f17454
Greg Doud <gpdoud@gmail.com>
2021-07-14 11:33:57 -0400

    Committed by GitUtil

---
+++ TestAppSettingsJson/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using System;
+using System.Configuration;
+
+namespace TestAppSettingsJson {
+    class Program {
+        static void Main(string[] args) {
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                            .Build();
+            var data = config["files:infile"];
+
+            var path = System.IO.Directory.GetCurrentDirectory();
+            var filename = "appsettings.json";
+            var fullpath = System.IO.Path.Combine(path, filename);
+            Console.WriteLine($"Fullpath is {fullpath}");
+        }
+    }
+}
